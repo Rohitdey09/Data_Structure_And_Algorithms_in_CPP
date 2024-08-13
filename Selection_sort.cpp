4d9baa3c160a5299eec5decec586967e80f69ce4
Rohit Dey <102870148+Rohitdey09@users.noreply.github.com>
2024-08-13 20:02:32 +0530

    Add files via upload

---
+++ Selection_sort.cpp
@@ -0,0 +1,36 @@
+// Implement selection sort to sort an array of integers in ascending order
+
+#include<iostream>
+
+using namespace std;
+
+int SelectionSort(int* arr,int n){
+    for(int i=0;i<=n-2;i++){
+        for(int j=i+1;j<=n-1;j++){
+            if(arr[i]>arr[j]){
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    SelectionSort(arr,n);
+
+    cout << "The sorted array is";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
+
